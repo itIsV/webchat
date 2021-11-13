8f2e82646e76f12559ab2c78ed29bd2ca54b2730
vinicios <soicinivt@gmail.com>
2021-11-13 04:13:29 -0300

    Logic for set placeholder value, to use in online-user and some of css.

--- src/views/js/chatRoom.js
+++ src/views/js/chatRoom.js
@@ -1,10 +1,24 @@
 const socket = window.io();
+const setPlaceholder = (value) => {
+  const nicknameInput = document.querySelectorAll('input')[0];
+  console.log('oi fora do if');
+  
+  if (!value) {
+    const aValue = document.querySelectorAll('input')[0].value;
+    console.log('dentro do if aValue: ', aValue);
+    nicknameInput.value = '';
+    return nicknameInput.setAttribute('placeholder', aValue);
+  }
+
+  nicknameInput.setAttribute('placeholder', value);
+};
+
 socket.on('connect', () => {
   const nickname = `User-${socket.id.slice(9)}`;
-  
+  setPlaceholder(nickname);
+
   document.addEventListener('submit', (e) => {
-    console.log(nickname);
-    const input = document.querySelector('input');
+    const input = document.querySelectorAll('input')[1];
     const chatMessage = input.value;
     e.preventDefault();
     socket.emit('message', { chatMessage, nickname });
@@ -13,6 +27,7 @@ socket.on('connect', () => {
   socket.on('message', (formattedMessage) => {
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
+    li.setAttribute('data-testid', 'message');
     li.innerText = formattedMessage;
     ul.appendChild(li);
   });
